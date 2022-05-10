e8ef2956ab03e0f4425e3d08e95fae372131876e
agostinho.dev <agostinho.developer@gmail.com>
2022-05-10 09:15:31 -0300

    feat: :sparkles: Updates some styles at Button

--- src/components/Theme/Button/styles.ts
+++ src/components/Theme/Button/styles.ts
@@ -1,13 +1,20 @@
+import Constants from "expo-constants";
 import styled from "styled-components/native";
 
 export const ButtonPersonalizated = styled.TouchableOpacity`
 
-    background-color: red;
+    background-color: ${Constants.manifest.extra.colors.five};
     margin: 20px;
     margin-bottom: 40px;
     border-radius: 10px;
     height: 45px;
+    justify-content: center;
+    align-items: center;
 
 `;
 
-export const ButtonPersonalizatedText = styled.Text``;
+export const ButtonPersonalizatedText = styled.Text`
+
+    font-family: 'Poppins_600SemiBold';  
+    font-size: 16px;
+`;
