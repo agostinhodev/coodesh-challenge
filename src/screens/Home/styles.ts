af286e821af074b5c219624b2e5aa8ed68dd2d73
agostinho.dev <agostinho.developer@gmail.com>
2022-05-09 23:26:54 -0300

    feat: :sparkles: Added some styles

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,12 +1,17 @@
+import Constants from "expo-constants";
+import { KeyboardAwareFlatList } from 'react-native-keyboard-aware-scroll-view';
 import styled from "styled-components/native";
-
-export const Container = styled.View`
-    display: flex;
-    flex: 1;
-    justify-content:center ;
-    align-items: center;
-`;
 
 export const Title = styled.Text`
     font-size: 16px;
+`;
+
+export const Container = styled(KeyboardAwareFlatList).attrs({
+    showsVerticalScrollIndicator: false,
+    behavior: 'padding',
+    enabled: true
+})`
+    background-color: ${Constants?.manifest?.extra?.colors?.one}; 
+    padding-top: 50px;
+    padding-bottom: 100px;
 `;
