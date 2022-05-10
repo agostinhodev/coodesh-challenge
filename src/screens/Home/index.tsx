8d7e20884a5b3e0fc6a24c102cbac31efee322a4
agostinho.dev <agostinho.developer@gmail.com>
2022-05-09 23:28:00 -0300

    feat:

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import { AxiosError } from 'axios';
+import React, { useEffect } from 'react';
 import Input from '../../components/Theme/Input';
+import UserItem from '../../components/UserItem';
+import { useUser } from '../../contexts/UserContext';
+import { User } from '../../models/Response/UserResponse';
 import { Container } from './styles';
+
 const Home: React.FC = () => {
+    const { fetch, isFetchingData, users } = useUser();
+
+    useEffect(() => {
+        fetch().catch((error: AxiosError | any) => {
+            console.log(error);
+        });
+    }, []);
+
     return (
-        <Container>
-            <Input keyboardType="default" autoCapitalize="none" />
-        </Container>
+        <Container
+            ListHeaderComponent={<Input keyboardType="default" autoCapitalize="none" />}
+            data={users?.results}
+            keyExtractor={(user: User) => user.login.uuid}
+            renderItem={(user: User) => <UserItem data={user} />}
+        />
     );
 };
 
