e8187678c51ac814e4c4abb80c599cbf33cee679
agostinhodev <agostinho.developer@gmail.com>
2022-05-11 15:09:09 -0300

    feat: :sparkles: Added ViewButton for Tap for more

--- src/components/Theme/Footer/index.tsx
+++ src/components/Theme/Footer/index.tsx
@@ -3,19 +3,20 @@ import { useUser } from '../../../contexts/UserContext';
 import LoadingActivity from '../../Loading';
 import Button from '../Button';
 import PersonalizatedModal from '../Modal';
+import { ViewButton } from './styles';
 
 const Footer: React.FC = () => {
     const { currentUser, fetch, isFetchingData } = useUser();
 
     return (
-        <>
+        <ViewButton>
             {isFetchingData ? (
                 <LoadingActivity title="Loading more..." />
             ) : (
                 <Button onPress={() => fetch()} title="Tap to load more..." />
             )}
             {currentUser && <PersonalizatedModal />}
-        </>
+        </ViewButton>
     );
 };
 
--- src/components/Theme/Header/styles.ts
+++ src/components/Theme/Header/styles.ts
@@ -1,7 +1,7 @@
 import { Picker } from '@react-native-picker/picker';
-import Constants from "expo-constants";
-import styled from "styled-components/native";
-
+import Constants from 'expo-constants';
+import styled from 'styled-components/native';
+import { Platform } from 'react-native';
 
 export const Container = styled.View`
     justify-content: center;
@@ -12,10 +12,9 @@ export const Container = styled.View`
 export const FormView = styled.View`
     flex-direction: row;
     justify-content: center;
-    align-items:center ;
+    align-items: center;
     padding-left: 20px;
     padding-right: 20px;
-
 `;
 
 export const ButtonFilter = styled.TouchableOpacity`
@@ -33,14 +32,13 @@ export const ViewModalFilters = styled.View`
     flex: 1;
     margin-top: 100px;
     background-color: ${Constants?.manifest?.extra?.colors?.six};
-    justify-content:flex-start;
+    justify-content: flex-start;
     align-items: center;
 `;
 
 export const CloseView = styled.View`
-    
     justify-content: flex-end;
-    align-items:flex-end ;
+    align-items: flex-end;
     width: 100%;
     z-index: -1;
     padding: 10px;
@@ -61,8 +59,10 @@ export const Description = styled.Text`
 
 export const PersonalizatedPicker = styled(Picker).attrs({})`
     min-width: 100%;
-    border-width: 1px;
-    background-color:  ${Constants.manifest?.extra?.colors.seven};
+    border-width: 0.2rem;
+    background-color: ${Platform.OS === 'ios'
+        ? Constants.manifest?.extra?.colors.six
+        : Constants.manifest?.extra?.colors.seven};
 `;
 
 export const ViewButton = styled.View`
